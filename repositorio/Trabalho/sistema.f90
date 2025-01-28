e45d3a79e5ede426f3d66d50bf0b8b2e94de9fd6
Kaio <kaio.chagas@ufv.br>
2025-01-27 22:05:47 -0300

    Definindo parâmetros de entrada

--- repositorio/Trabalho/sistema.f90
+++ repositorio/Trabalho/sistema.f90
@@ -1,2 +1,10 @@
 module sistema
     implicit none
+
+contains
+    subroutine sistema (t, t_inf, dydt, y, t_rec)
+        real(8), intent(in) :: t
+        real(8), intent(in) :: t_inf
+        real(8), intent(in) :: t_rec
+        real(8), intent(in) :: y(3)
+    
