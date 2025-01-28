49d20db63d17c76a0786d5fdd4155e8979d65a5d
Kaio <kaio.chagas@ufv.br>
2025-01-27 22:19:13 -0300

    Equações do sistema de equações diferenciais

--- repositorio/Trabalho/sistema.f90
+++ repositorio/Trabalho/sistema.f90
@@ -3,12 +3,16 @@ module sistema
 
 contains
     subroutine sistema (t, t_inf, dydt, y, t_rec)
+        
         !Parâmetros de entrada
         real(8), intent(in) :: t
         real(8), intent(in) :: t_inf
         real(8), intent(in) :: t_rec
         real(8), intent(in) :: y(3)
         
+        !Parâmetros de saída
         real(8), intent(out) :: dydt
 
-        
+        dydt(1) = -t_inf * y(1) * y(2)
+        dydt(2) = t_inf * y(1) * y(2) - t_rec * y(2)
+        dydt(3) = t_rec * y(2)
