1f634decb296abe98c5210f4478ccbcc056efe84
Kaio <kaio.chagas@ufv.br>
2025-01-28 16:13:09 -0300

    Fiz uma correção besta e o erro sumiu

--- repositorio/Trabalho/sistema.f90
+++ repositorio/Trabalho/sistema.f90
@@ -2,7 +2,7 @@ module sistema
     implicit none
 
 contains
-    subroutine sistema_e_d(t, t_inf, dydt, y, t_rec)
+    subroutine sistema_e_d(t, y, dydt, t_inf, t_rec)
         
         !Parâmetros de entrada
         real(8), intent(in) :: t
@@ -27,7 +27,7 @@ contains
         real(8), intent(in) :: t_inf
         real(8), intent(in) :: t_rec
         real(8), intent(in) :: y0(3)
-        real(8), intent(in) :: pt
+        integer, intent(in) :: pt
 
         !Parâmetros de saída
         real(8), intent(out) :: t_saida(pt)
@@ -40,7 +40,7 @@ contains
 
         h = (delta_t(2) - delta_t(1)) / (pt - 1)
 
-        t = delta_t
+        t = delta_t(1)
         y = y0
         t_saida(1) = t
         y_saida(:, 1) = y
@@ -48,10 +48,10 @@ contains
         !Para resolver o sistema em todos os passos de tempo
         do i = 2, pt
             !Método de Runge-Kutta de 4 ordem
-            call kutta_sistema(t, y, k1, t_inf, t_rec)
-            call kutta_sistema(t + 0.5*h, y + 0.5*h*k1, k2, t_inf, t_rec)
-            call kutta_sistema(t + 0.5*h, y + 0.5*h*k2, k3, t_inf, t_rec)
-            call kutta_sistema(t + h, y + h*k3, k4, t_inf, t_rec)
+            call sistema_e_d(t, y, k1, t_inf, t_rec)
+            call sistema_e_d(t + 0.5*h, y + 0.5*h*k1, k2, t_inf, t_rec)
+            call sistema_e_d(t + 0.5*h, y + 0.5*h*k2, k3, t_inf, t_rec)
+            call sistema_e_d(t + h, y + h*k3, k4, t_inf, t_rec)
 
             y = y + (h/6.0)*(k1 + 2*k2 + 2*k3 + k4)
             t = t + h
