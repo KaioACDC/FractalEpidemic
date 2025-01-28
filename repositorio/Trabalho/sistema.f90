797109f580198fdfa97a99e6c521614dbcce895e
Kaio <kaio.chagas@ufv.br>
2025-01-27 22:12:47 -0300

    Definindo parâmetros de saída

--- repositorio/Trabalho/sistema.f90
+++ repositorio/Trabalho/sistema.f90
@@ -3,8 +3,12 @@ module sistema
 
 contains
     subroutine sistema (t, t_inf, dydt, y, t_rec)
+        !Parâmetros de entrada
         real(8), intent(in) :: t
         real(8), intent(in) :: t_inf
         real(8), intent(in) :: t_rec
         real(8), intent(in) :: y(3)
-    
+        
+        real(8), intent(out) :: dydt
+
+        
